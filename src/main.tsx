1d5aa824462ca680063dfada116272285a4cce03
Alfredo Sobrados <alfredo.118s347@gmail.com>
2025-08-31 22:42:03 +0200

    refactor: improved favicon icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import logoUrl from '@/assets/LOGO.png'
 import { Analytics } from '@vercel/analytics/react'
-
-// Set favicon to the project logo (PNG) without distortion
-const ensureFavicon = (href: string) => {
-  let link = document.querySelector<HTMLLinkElement>('link[rel="icon"]');
-  if (!link) {
-    link = document.createElement('link');
-    link.rel = 'icon';
-    link.type = 'image/png';
-    document.head.appendChild(link);
-  }
-  link.href = href;
-};
-
-ensureFavicon(logoUrl);
 
 createRoot(document.getElementById("root")).render(
   <>
